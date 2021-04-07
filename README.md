8bc6ffaa65305ffc28fe904a71e2a85716bb5c4c
NaiduVeeraVishnuVardhan <60171846+NaiduVeeraVishnuVardhan@users.noreply.github.com>
2021-04-08 04:47:11 +1000

    JWT authentication added

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # justForFun
 
 This is a "User registration and Login Module" developed using JavaScript, NodeJs, Mongodb.
+
+Pre-requisites:
+Install JavaScript, NodeJs, Mongodb, bodyparser. 
+packages used: "bcryptjs", "body-parser", "express", "mongoose". 
--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const User = require('./model/user')
 const bcrypt = require('bcryptjs')
-
+const { json } = require('body-parser')
+const jwt = require("jsonwebtoken")
+const JWT_SECRET ='fiuhberuipvhbrtuipghewr9p7eg982370237480uobcn;kwcie'
 mongoose.connect('mongodb://localhost:27017/login-app-db',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -15,17 +17,91 @@ const app = express()
 
 app.use('/', express.static(path.join(__dirname,'static')))
 app.use(bodyParser.json())
+//--------------------------------Change password-----------------------------------------------------------------
+app.post('/api/change-password', async(req,res)=>{
+    const {token, newpassword: plainTextPassword} = req.body
+    if (!plainTextPassword ||typeof plainTextPassword !== 'string')
+    {
+        return res.json({status: 'error', error: 'INVALID USERNAME'})
+    }
+    if (plainTextPassword.length < 5){
+        return res.json({
+            status: "error",
+            error : "password must be more than 6 characters"
+        })
+    }
+    
+         
+    try{
 
-
-app.post('/api/register', async (req, res) => {
-    const { username, password } = req.body
-    const password_encrypted = await bcrypt.hash(password,10)
-    // console.log(await bcrypt.hash(password,10))
-    console.log(password_encrypted)
-    res.json({ status:'ok'})
+        const user = jwt.verify(token, JWT_SECRET)  
+        const _id = user.id
+        const password = await bcrypt.hash(plainTextPassword,10)
+        await User.updateOne({ _id},{$set:{password}})
+        res.json({status:'ok'})
+        
+    }
+    catch(error){
+        res.json({status: 'error', error:"Someone is tring to mess up "})
+    }
+    
 })
+//----------------------------------Login---------------------------------------------------------------
+app.post('/api/login', async(req,res)=>{
+
+    const{username, password} = req.body
+    
+    const user = await User.findOne({username}).lean()
+    if(!user){
+        return res.json({status:'error', error:'INVALID USERNAME?PASSWORD'})
+    }
+
+    if(await bcrypt.compare(password, user.password)){
+        const token = jwt.sign( {id: user._id, username: user.username} , JWT_SECRET )
+        return res.json({status:'ok', data:token})
+    }
+
+    res.json({status:'error', data:"error:'INVALID USERNAME?PASSWORD'"})
+}
+)
+//-------------------------------------Register------------------------------------------------------------
+app.post('/api/register', async (req, res) => {
+
+    const { username, password: plainTextPassword } = req.body
 
 
+    if (!username ||typeof username !== 'string')
+    {
+        return res.json({status: 'error', error: 'INVALID USERNAME'})
+    }
+    if (!plainTextPassword ||typeof plainTextPassword !== 'string')
+    {
+        return res.json({status: 'error', error: 'INVALID USERNAME'})
+    }
+    if (plainTextPassword.length < 5){
+        return res.json({
+            status: "error",
+            error : "password must be more than 6 characters"
+        })
+    }
+
+    const password = await bcrypt.hash(plainTextPassword,10)
+        try{
+        const response = await User.create({
+            username,
+            password
+        })
+        console.log('User created successfully: ',response)
+        
+    } catch (error) {
+        if(error.code === 11000){
+            return res.json({status: 'error', error: 'Username already in use'})
+        }
+        throw error
+    }
+    res.json({ status:'ok'})
+})   
 app.listen(9999,() => {
     console.log('Server up at 9999')
 })
+
